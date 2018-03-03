8d34542934f68097673d4ab9685d021989b7e821
ALKONDR <aalexkondr@gmail.com>
2018-03-04 02:50:36 +0300

    add basic Order class

---
+++ Order.js
@@ -0,0 +1,9 @@
+export default class Order {
+    constructor() {
+        this.numberOfPeople = "2";
+        this.numberOfDays = "5";
+        this.setName = "Классическое";
+        this.setContent = [];
+        this.customerName = "Kek Lolovich";
+    }
+}
