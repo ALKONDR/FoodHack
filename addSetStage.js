32474785a3d5d1c5190ef095a633389a35c525ea
ALKONDR <aalexkondr@gmail.com>
2018-03-04 06:43:23 +0300

    add add set stage middleware

---
+++ addSetStage.js
@@ -0,0 +1,8 @@
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const Scene = require('telegraf/scenes/base');
+const { enter, leave } = Stage;
+
+module.exports = function addSetStage() {
+
+};
